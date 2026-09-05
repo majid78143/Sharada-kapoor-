4445d6c89361075f9abb9918a1e5d77aac4af6e1
Majid <mdmajidansari33640@gmail.com>
2026-09-05 22:08:35 +0530

    Add files via upload

---
+++ src/services.js
@@ -0,0 +1,11 @@
+const REGIONS = ["IND", "BD", "PK", "SG", "ME", "BR"];
+const AUTO_LIKE_DAYS = [15, 30];
+const PANEL_HOURS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 24, 48, 72, 168, 216, 744];
+const GUILD_ACTIONS = ["restart", "stop", "delete", "get-glory", "get-clan-info"];
+
+module.exports = {
+  REGIONS,
+  AUTO_LIKE_DAYS,
+  PANEL_HOURS,
+  GUILD_ACTIONS
+};
